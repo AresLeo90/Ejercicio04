ae32d4225dc24527d64c04a36052a2ab4927135f
Ares Leonardo <ares.leonardo@outlook.es>
2018-06-29 13:31:52 +0200

    Ejemplo de reflection funcionando. close #3

--- Ejercicio04/Alumno.cs
+++ Ejercicio04/Alumno.cs
@@ -1,10 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using static EJ_Reflection.Program;
+using System.Reflection;
+
 
 namespace EJ_Reflection
 {
-    class Alumno
-    {
+    public class Alumno
+         {
+            public int IdAlumno { get; set; }
+            public string Nombre { get; set; }
+            public string Apellidos { get; set; }
+            public string Dni { get; set; }
+
+            public Alumno()
+            {
+            }
+
+        public Alumno(int IdAlumno, string Nombre, string Apellidos, string Dni)
+            {
+            this.IdAlumno = IdAlumno;
+            this.Nombre = Nombre;
+            this.Apellidos = Apellidos;
+            this.Dni = Dni;
+             }
+        }
     }
-}
--- Ejercicio04/Program.cs
+++ Ejercicio04/Program.cs
@@ -1,11 +1,28 @@
 ﻿using System;
+using System.Reflection;
+using static EJ_Reflection.Program;
 
-namespace Ejercicio04
+
+namespace EJ_Reflection
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Assembly myAssembly = typeof(Program).Assembly;
+            //get type of class Calculator from just loaded assembly
+            Type alumnoType = myAssembly.GetType("EJ_Reflection.Alumno");
+
+            //create  instance  of  class Calculator
+            object objetoDeAlumno = Activator.CreateInstance(alumnoType, 1,"Ares","Ordoñez","3243434x");
+            //Console.WriteLine(((Alumno)objetoDeAlumno).Nombre);
+
+            //((Alumno)alumno).Nombre
+            /*  Alummno miAlumno = (Alumno)alumno;
+             *  miAlumno.Nombre = "Ares";*/
+
+            Alumno miAlumno = (Alumno)objetoDeAlumno;
+            Console.WriteLine(miAlumno.Nombre);
 
         }
     }
